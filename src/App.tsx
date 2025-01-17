e5f0094ec54f1c68f440ac49e99b05dba6ec90e7
Lavesh Chaudhary <laveshchauhdary61@gmail.com>
2025-01-17 15:01:16 +0000

    Add basic layout and structure for CRM interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import dpsLogo from './assets/DPS.svg';
 import './App.css';
 
-function App() {
+function App(): JSX.Element {
 	return (
-		<>
-			<div>
-				<a href="https://www.digitalproductschool.io/" target="_blank">
+		<div className="container">
+			<div className="header">
+				<a href="https://www.digitalproductschool.io/" target="_blank" rel="noopener noreferrer">
 					<img src={dpsLogo} className="logo" alt="DPS logo" />
 				</a>
 			</div>
-			<div className="home-card">
-				<p>Your solution goes here 😊</p>
+			<div className="main-content">
+				{/* Search and filter section will go here */}
+				<div className="filters">
+				</div>
+
+				{/* User table will go here */}
+				<div className="table-container">
+				</div>
 			</div>
-		</>
+		</div>
 	);
 }
 
